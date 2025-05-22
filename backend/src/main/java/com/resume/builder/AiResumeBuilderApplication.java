a207f1278be2e2b9d2599273fbfafaa86b6a1826
SoniSanskar1 <sanskar.soni369@gmail.com>
2025-05-22 20:51:00 +0530

    removed env loader class

--- backend/src/main/java/com/resume/builder/AiResumeBuilderApplication.java
+++ backend/src/main/java/com/resume/builder/AiResumeBuilderApplication.java
@@ -3,12 +3,9 @@ package com.resume.builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.resume.builder.config.DotenvInitializer;
-
 @SpringBootApplication
 public class AiResumeBuilderApplication {
     public static void main(String[] args) {
-        DotenvInitializer dotenv = new DotenvInitializer();
         SpringApplication.run(AiResumeBuilderApplication.class, args);
     }
 }
